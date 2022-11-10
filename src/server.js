867a31b9ca644ae798e1d193385c487558162281
rafaelitajahy <rafaelitajahy@gmail.com>
2022-11-10 15:57:23 -0300

    feat: initial configuration server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, resp) => {
+  return resp.status(200).send();
+});
+
+app.listen(3333, () => console.log('Server is starting!'));
